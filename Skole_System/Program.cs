0e784e16b6313990bfa2392f8d71516b5d07dda9
Salar Amir <salar831@gmail.com>
2023-01-31 12:18:22 +0100

    initial commit

---
+++ Skole_System/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Skole_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
